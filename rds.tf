f8760e037c9f242a74fa645862b5d9acba6e006e
03Aziz02 <157199305+03Aziz02@users.noreply.github.com>
2024-02-24 19:54:50 +0530

    Create rds.tf

---
+++ rds.tf
@@ -0,0 +1,21 @@
+resource "aws_db_subnet_group" "rds-sub" {
+  name       = "rds-sub"
+  subnet_ids = [aws_subnet.privatesubnet3.id, aws_subnet.privatesubnet4.id]
+
+  tags = {
+    Name = "My DB subnet group"
+  }
+}
+
+resource "aws_db_instance" "rds" {
+  allocated_storage      = 10
+  engine                 = "mysql"
+  db_subnet_group_name   = aws_db_subnet_group.rds-sub.name
+  engine_version         = "8.0.35"
+  instance_class         = "db.t2.micro"
+  multi_az               = true
+  username               = "admin123"
+  password               = "aziz1234"
+  skip_final_snapshot    = true
+  vpc_security_group_ids = [aws_security_group.databasesg.id]
+}
