3f9550b7d5037d443188fdecc7f30e404ef2d1d7
03Aziz02 <157199305+03Aziz02@users.noreply.github.com>
2024-02-25 12:38:21 +0530

    Update publicsubnet2.tf

--- publicsubnet2.tf
+++ publicsubnet2.tf
@@ -1,8 +1,8 @@
-resource "aws_subnet" "publicsubnet2" {
-  vpc_id     = aws_vpc.main.id
+resource "aws_subnet" "public-subnet2" {
+  vpc_id     = aws_vpc.aziz.id
   cidr_block = "10.0.2.0/24"
 
   tags = {
-    Name = "publicsubnet2"
+    Name = "public-subnet2"
   }
 }
