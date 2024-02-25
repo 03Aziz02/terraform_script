b12843b9bc1b53f2e4452954bd95f54eceae617c
03Aziz02 <157199305+03Aziz02@users.noreply.github.com>
2024-02-25 22:32:08 +0530

    Update data2.sh

--- data2.sh
+++ data2.sh
@@ -5,5 +5,5 @@ sudo systemctl start httpd
 sudo systemctl enable httpd
 sudo yum -y install git
 git clone https://github.com/03Aziz02/food.git
-sudo mv food/ /var/www/html
+sudo mv food/* /var/www/html
 sudo systemctl restart httpd
