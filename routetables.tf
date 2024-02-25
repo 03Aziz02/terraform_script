d671cabe970eefb8543cdad070d0772a97d9bb8c
03Aziz02 <157199305+03Aziz02@users.noreply.github.com>
2024-02-25 12:45:47 +0530

    Update routetables.tf

--- routetables.tf
+++ routetables.tf
@@ -1,22 +1,22 @@
 resource "aws_route_table" "route" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.aziz.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gateway.id
+    gateway_id = aws_internet_gateway.aziz-gateway.id
   }
 
   tags = {
     Name = "route_to_internet"
   }
 }
-
+#route 1
 resource "aws_route_table_association" "route-1" {
-  subnet_id      = aws_subnet.publicsubnet1.id
+  subnet_id      = aws_subnet.public-subnet1.id
   route_table_id = aws_route_table.route.id
 }
-
+#route 2
 resource "aws_route_table_association" "route-2" {
-  subnet_id      = aws_subnet.publicsubnet2.id
+  subnet_id      = aws_subnet.public-subnet2.id
   route_table_id = aws_route_table.route.id
 }
