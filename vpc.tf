f19d7e7ef0e38208eb26587a348eef66ad1d7b7d
03Aziz02 <157199305+03Aziz02@users.noreply.github.com>
2024-02-25 22:04:56 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,7 @@
 resource "aws_vpc" "aziz" {
-  cidr_block       = "10.0.0.0/16"
+  cidr_block       = var.vpc_cidr
   instance_tenancy = "default"
-
   tags = {
-    Name = "aziz"
+    Name = "aziz-vpc"
   }
 }
