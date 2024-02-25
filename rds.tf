752e29b1ed08a713e977b17e687fff853b8ba30d
03Aziz02 <157199305+03Aziz02@users.noreply.github.com>
2024-02-25 22:34:44 +0530

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -1,16 +1,16 @@
-resource "aws_db_subnet_group" "rds-sub" {
-  name       = "rds-sub"
-  subnet_ids = [aws_subnet.private-subnet3.id, aws_subnet.private-subnet4.id]
+resource "aws_db_subnet_group" "rds-subnet" {
+  name       = "rds-subnet"
+  subnet_ids = [aws_subnet.private-subnet1.id, aws_subnet.private-subnet2.id]
 
   tags = {
     Name = "My DB subnet group"
   }
 }
 
-resource "aws_db_instance" "rds" {
+resource "aws_db_instance" "rds-subnet" {
   allocated_storage      = 10
   engine                 = "mysql"
-  db_subnet_group_name   = aws_db_subnet_group.rds-sub.name
+  db_subnet_group_name   = aws_db_subnet_group.rds-subnet.id
   engine_version         = "8.0.35"
   instance_class         = "db.t2.micro"
   multi_az               = true
