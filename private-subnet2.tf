1ba27bf190255334f820cc9009a37628427c9ed8
03Aziz02 <157199305+03Aziz02@users.noreply.github.com>
2024-02-25 22:12:51 +0530

    Update private-subnet2.tf

--- private-subnet2.tf
+++ private-subnet2.tf
@@ -1,6 +1,8 @@
 resource "aws_subnet" "private-subnet2" {
-  vpc_id     = aws_vpc.aziz.id
-  cidr_block = "10.0.4.0/24"
+  vpc_id                  = aws_vpc.aziz.id
+  cidr_block              = "10.0.4.0/24"
+  map_public_ip_on_launch = false
+  availability_zone       = "us-east-1b"
 
   tags = {
     Name = "private-subnet2"
