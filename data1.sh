006924b2caf89e1df3f05ccec91edbb0335b5971
03Aziz02 <157199305+03Aziz02@users.noreply.github.com>
2024-02-25 10:47:59 +0530

    Update data1.sh

--- data1.sh
+++ data1.sh
@@ -4,4 +4,5 @@ sudo systemctl start httpd
 sudo systemctl enable httpd
 sudo yum -y install git
 git clone https://github.com/03Aziz02/ecomm.git
+sudo mv ecomm/ /var/www/html/
 sudo systemctl restart httpd
