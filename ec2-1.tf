88f1383278b06b3af9565f55ad1a867619e2bb1d
03Aziz02 <157199305+03Aziz02@users.noreply.github.com>
2024-02-25 14:14:11 +0530

    Update ec2-1.tf

--- ec2-1.tf
+++ ec2-1.tf
@@ -2,8 +2,8 @@ resource "aws_instance" "ecomm" {
   ami                         = "ami-07761f3ae34c4478d"
   instance_type               = "t2.micro"
   key_name                    = "MK"
-  vpc_security_group_ids      = [aws_security_group.websg.id]
-  subnet_id                   = aws_subnet.publicsubnet1.id
+  vpc_security_group_ids      = [aws_security_group.web-sg.id]
+  subnet_id                   = aws_subnet.public-subnet1.id
   associate_public_ip_address = true
   user_data                   = file("data1.sh")
 
