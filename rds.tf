be5a68d7c50cf01bc3529f74460d4da6952ec3c1
03Aziz02 <157199305+03Aziz02@users.noreply.github.com>
2024-02-25 14:19:05 +0530

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -1,6 +1,6 @@
 resource "aws_db_subnet_group" "rds-sub" {
   name       = "rds-sub"
-  subnet_ids = [aws_subnet.privatesubnet3.id, aws_subnet.privatesubnet4.id]
+  subnet_ids = [aws_subnet.private-subnet3.id, aws_subnet.private-subnet4.id]
 
   tags = {
     Name = "My DB subnet group"
@@ -14,8 +14,8 @@ resource "aws_db_instance" "rds" {
   engine_version         = "8.0.35"
   instance_class         = "db.t2.micro"
   multi_az               = true
-  username               = "admin123"
+  username               = "admin"
   password               = "aziz1234"
   skip_final_snapshot    = true
-  vpc_security_group_ids = [aws_security_group.databasesg.id]
+  vpc_security_group_ids = [aws_security_group.db-sg.id]
 }
