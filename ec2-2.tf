86ecdbe1a83cb340e98e2898a44f40d75358eb54
03Aziz02 <157199305+03Aziz02@users.noreply.github.com>
2024-02-25 14:14:38 +0530

    Update ec2-2.tf

--- ec2-2.tf
+++ ec2-2.tf
@@ -2,8 +2,8 @@ resource "aws_instance" "food" {
   ami                         = "ami-07761f3ae34c4478d"
   instance_type               = "t2.micro"
   key_name                    = "MK"
-  vpc_security_group_ids      = [aws_security_group.websg.id]
-  subnet_id                   = aws_subnet.publicsubnet2.id
+  vpc_security_group_ids      = [aws_security_group.web-sg.id]
+  subnet_id                   = aws_subnet.public-subnet2.id
   associate_public_ip_address = true
   user_data                   = file("data2.sh")
 
